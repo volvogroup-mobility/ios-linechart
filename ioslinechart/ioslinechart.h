d21d4b42e102ce74fedded37ea58419ed652af6c
Timothy G. Rundle <timothy.rundle@volvo.com>
2016-05-13 09:37:21 -0400

    added dynamic framework target (required gitsubtrees of ios-versioncheck and uikit-utils) as they are not Carthage friendly

---
+++ ioslinechart/ioslinechart.h
@@ -0,0 +1,24 @@
+//
+//  ios-linechart.h
+//  ios-linechart
+//
+//  Created by Timothy G. Rundle on 5/12/16.
+//  Copyright © 2016 Marcel Ruegenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ios-linechart.
+FOUNDATION_EXPORT double ios_linechartVersionNumber;
+
+//! Project version string for ios-linechart.
+FOUNDATION_EXPORT const unsigned char ios_linechartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ioslinechart/PublicHeader.h>
+
+
+#import <ioslinechart/ChartAppDelegate.h>
+#import <ioslinechart/ChartViewController.h>
+#import <ioslinechart/LCInfoView.h>
+#import <ioslinechart/LCLegendView.h>
+#import <ioslinechart/LCLineChartView.h>
